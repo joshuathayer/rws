c1193b038d4e1e54f98b814dd6c3716e4fb6fe6f
Joshua Thayer <jthayer@mit.edu>
2020-06-23 00:51:55 -0700

    wip, inverse fft working

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate rustfft;
 
 use hound;
 // use std::f32::consts::PI;
+use std::cmp;
 use std::i16;
 
 // use rustfft::FFT;
@@ -19,21 +20,26 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
 
-// fn write_wav() -> () {
-//     let spec = hound::WavSpec {
-//         channels: 1,
-//         sample_rate: 44100,
-//         bits_per_sample: 16,
-//         sample_format: hound::SampleFormat::Int,
-//     };
-//     let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
-//     for t in (0..44100).map(|x| x as f32 / 44100.0) {
-//         let sample = (t * 440.0 * 2.0 * PI).sin();
-//         let amplitude = i16::MAX as f32;
-//         writer.write_sample((sample * amplitude) as i16).unwrap();
-//     }
-//     writer.finalize().unwrap();
-// }
+fn write_wav(bytes: &Vec<u16>, t: usize) -> () {
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 12000,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    let mut writer = hound::WavWriter::create(format!("out-{}.wav", t), spec).unwrap();
+
+    for b in bytes {
+        writer.write_sample(*b as i16).unwrap();
+    }
+    // for t in (0..44100).map(|x| x as f32 / 44100.0) {
+    //     let sample = (t * 440.0 * 2.0 * PI).sin();
+    //     let amplitude = i16::MAX as f32;
+    //     writer.write_sample((sample * amplitude) as i16).unwrap();
+    // }
+    writer.finalize().unwrap();
+}
 
 fn spectra_to_bitmap(spectra: &[Vec<Complex<f32>>]) -> Vec<Vec<(u8, u8, u8, u8)>> {
     let height = spectra.len() as u32;
@@ -64,7 +70,7 @@ fn spectra_to_bitmap(spectra: &[Vec<Complex<f32>>]) -> Vec<Vec<(u8, u8, u8, u8)>
 
 fn add_candidates_to_bitmap(
     bm: &mut Vec<Vec<(u8, u8, u8, u8)>>,
-    candidates: &Vec<&(usize, usize, f32)>,
+    candidates: &Vec<(usize, usize, f32)>,
 ) -> () {
     let mut max: f32 = 0.0;
 
@@ -244,37 +250,35 @@ fn find_costas_powers(
     result
 }
 
-fn choppy(fname: &str) -> () {
-    let mut reader = hound::WavReader::open(fname).expect("Failed to open WAV file");
-    let spec = reader.spec();
+fn costas(
+    signal: &Vec<num::Complex<f32>>,
+    sample_len: u32,
+    sample_rate: u32,
+) -> Vec<(usize, usize, f32)> {
+    // reader: &mut hound::WavReader<std::io::BufReader<std::fs::File>>
 
-    println!("Read rate {}", spec.sample_rate);
+    println!("Read rate {}", sample_rate);
     println!(
         "Samples {} so time {}s",
-        reader.len(),
-        reader.len() / spec.sample_rate
+        sample_len,
+        sample_len / sample_rate
     );
-    let chopsize = (spec.sample_rate as f32 * 0.16) as u32;
-    let stepsize = (spec.sample_rate as f32 * 0.04) as u32;
+    let chopsize = (sample_rate as f32 * 0.16) as u32;
+    let stepsize = (sample_rate as f32 * 0.04) as u32;
 
-    let chopcount = (reader.len() / stepsize) - 3;
+    let chopcount = (sample_len / stepsize) - 3;
 
     // this is the width in Hz of each element in the FFT result
     // our nyquist freq is sample_rate / 2, so 6kHz
     // our FFT will have `chopsize` * 2 bins (`chopsize` usable)
-    let binwidth = (spec.sample_rate as f32 / 2.0) / chopsize as f32;
+    let binwidth = (sample_rate as f32 / 2.0) / chopsize as f32;
 
     println!(
         "Covering {}Hz over {} bins, so {} Hz per bin",
-        spec.sample_rate / 2,
+        sample_rate / 2,
         chopsize,
         binwidth
     );
-
-    let signal = reader
-        .samples::<i16>()
-        .map(|x| Complex::new(x.unwrap() as f32, 0f32))
-        .collect::<Vec<_>>();
 
     println!("building spectra...");
 
@@ -321,12 +325,13 @@ fn choppy(fname: &str) -> () {
 
     let median = ordered[(res.len() / 2) as usize];
 
-    // let threshold = 10.0;
     let threshold = median.2 * 1.5;
     println!("threshold {}", threshold);
+
     let candidates = res
         .iter()
         .filter(|(_, _, e)| (*e / mean) > threshold)
+        .copied()
         .collect::<Vec<_>>();
 
     add_candidates_to_bitmap(&mut bm, &candidates);
@@ -337,37 +342,114 @@ fn choppy(fname: &str) -> () {
     println!("done");
 
     write_bitmap("waterfall.png", &bm);
+
+    candidates
 }
 
-fn find_spectral_peak(filename: &str) -> Option<f32> {
-    let mut reader = hound::WavReader::open(filename).expect("Failed to open WAV file");
-    let num_samples = reader.len() as usize;
+// fn fine_process(reader: &mut hound::WavReader<std::io::BufReader<std::fs::File>>) -> () {
+fn fine_process(
+    original_signal: &Vec<num::Complex<f32>>,
+    num_samples: u32,
+    sample_rate: u32,
+    candidates: &Vec<(usize, usize, f32)>, // (fq, time, power) triples
+) -> () {
+    let mut signal = original_signal.clone();
+
+    // fft of entire signal
     let mut planner = FFTplanner::new(false);
+    let mut iplanner = FFTplanner::new(true);
 
-    let sample_rate = reader.spec().sample_rate;
+    // one second of zeros
+    let zeros: Vec<Complex<f32>> = vec![Complex::zero(); sample_rate as usize];
 
-    let fft = planner.plan_fft(num_samples);
-
-    let mut signal = reader
-        .samples::<i16>()
-        .map(|x| Complex::new(x.unwrap() as f32, 0f32))
-        .collect::<Vec<_>>();
+    signal.extend_from_slice(&zeros);
     let mut spectrum = signal.clone();
+
+    let fft = planner.plan_fft(spectrum.len() as usize);
+    let ifft = iplanner.plan_fft(spectrum.len() as usize);
 
     fft.process(&mut signal[..], &mut spectrum[..]);
 
-    let max_peak = spectrum
-        .iter()
-        .take(num_samples / 2)
-        .enumerate()
-        .max_by_key(|&(_, freq)| freq.norm() as u32);
+    // ok, for each candidate base frequency f,
+    // we chop out f-6.25 to f+50 from the full fft
+    let binwidth = 3.125;
+    for (f, t, p) in candidates {
+        let min = cmp::max(0, f - ((6.25 / binwidth) as usize));
+        let max = f + ((50.0 / binwidth) as usize); // XXX jt overflow
 
-    if let Some((i, _)) = max_peak {
-        let bin = sample_rate as f32 / num_samples as f32;
-        Some(i as f32 * bin)
-    } else {
-        None
+        // xxx jt you are here
+        // how can we generate 16 seconds of signal with such a small slice of spectrum?
+        // let mut spectrum_slice: Vec<Complex<f32>> = vec![Complex::zero(); max - min];
+        let mut spectrum_slice_0: Vec<Complex<f32>> = vec![Complex::zero(); min];
+        let mut spectrum_slice_1: Vec<Complex<f32>> = vec![Complex::zero(); max - min];
+        let mut spectrum_slice_2: Vec<Complex<f32>> =
+            vec![Complex::zero(); signal.len() as usize - max];
+
+        let mut generated_signal = vec![Complex::zero(); signal.len()];
+
+        // spectrum_slice_1.copy_from_slice(&spectrum[min..max]);
+        // spectrum_slice_0.extend(spectrum_slice_1);
+        // spectrum_slice_0.extend(spectrum_slice_2);
+        let mut spectrum_copy: Vec<Complex<f32>> = spectrum.clone();
+
+        println!("Doing inverse...");
+        //ifft.process(&mut spectrum_slice_0, &mut generated_signal);
+        ifft.process(&mut spectrum_copy, &mut generated_signal);
+
+        let reals = generated_signal[..generated_signal.len() as usize]
+            .into_iter()
+            .map(|v| ((v.re / (generated_signal.len() as f32)) as u16))
+            .collect::<Vec<u16>>();
+
+        // let scaled = reals.map(|v| ((v / m) * 2.0) - 1.0);
+
+        write_wav(&reals, *f);
+
+        //println!("Did inverse. {} {:?}", generated_signal.len(), reals);
     }
+
+    // fn find_spectral_peak(filename: &str) -> Option<f32> {
+    //     let mut reader = hound::WavReader::open(filename).expect("Failed to open WAV file");
+    //     let num_samples = reader.len() as usize;
+    //     let mut planner = FFTplanner::new(false);
+
+    //     let sample_rate = reader.spec().sample_rate;
+
+    //     let fft = planner.plan_fft(num_samples);
+
+    //     let mut signal = reader
+    //         .samples::<i16>()
+    //         .map(|x| Complex::new(x.unwrap() as f32, 0f32))
+    //         .collect::<Vec<_>>();
+
+    //     let mut spectrum = signal.clone();
+
+    //     fft.process(&mut signal[..], &mut spectrum[..]);
+
+    //     let max_peak = spectrum
+    //         .iter()
+    //         .take(num_samples / 2)
+    //         .enumerate()
+    //         .max_by_key(|&(_, freq)| freq.norm() as u32);
+
+    //     if let Some((i, _)) = max_peak {
+    //         let bin = sample_rate as f32 / num_samples as f32;
+    //         Some(i as f32 * bin)
+    //     } else {
+    //         None
+    //     }
+    // }
+}
+
+fn choppy(fname: &str) -> (Vec<num::Complex<f32>>, u32, u32) {
+    let mut reader = hound::WavReader::open(fname).expect("Failed to open WAV file");
+
+    let signal = reader
+        .samples::<i16>()
+        .map(|x| Complex::new(x.unwrap() as f32, 0f32))
+        .collect::<Vec<_>>();
+
+    (signal, reader.len(), reader.spec().sample_rate)
 }
 
 fn main() {
@@ -379,9 +461,10 @@ fn main() {
     //     sqr_sum + sample * sample
     // });
 
-    let peak = find_spectral_peak("samples/FT8/191111_110115.wav").unwrap();
-    println!("Peak is {} Hz", peak);
+    // let peak = find_spectral_peak("samples/FT8/191111_110115.wav").unwrap();
+    // println!("Peak is {} Hz", peak);
 
+    let (signal, sample_len, rate) =
     // choppy("samples/FT8/181201_180245.wav");
     choppy("../ft8_lib/tests/191111_110130.wav");
     // choppy("../ft8_lib/tests/191111_110145.wav");
@@ -391,6 +474,11 @@ fn main() {
     // choppy("../ft8_lib/tests/191111_110215.wav");
 
     // choppy("samples/FT8/191111_110115.wav");
+    let mut candidates: std::vec::Vec<(usize, usize, f32)> = costas(&signal, sample_len, rate);
+    candidates.sort_by(|(_, _, l), (_, _, r)| r.partial_cmp(l).unwrap());
+    println!("{:?}", candidates);
+
+    fine_process(&signal, sample_len, rate, &candidates);
 
     // choppy("sine.wav");
 }
